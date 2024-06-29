b43ef0e6e95306e091d8702a31acac829123895d
Devin Lim <devinlim2025@gmail.com>
2024-06-28 22:09:28 -0400

    fixed data bug

--- makedata.py
+++ makedata.py
@@ -19,8 +19,8 @@ def zerocenter(array):
     array -= avg
     return array
 
-for i in np.arange(0, 2345-50, 14):
-    for j in np.arange(0, 2334-50, 10):
+for i in np.arange(0, 2345-50, 30):
+    for j in np.arange(0, 2334-50, 30):
         applicable_square = np.copy(applicable[i:i+50, j:j+50])
         applicable_squares = np.sum(applicable_square)
         if applicable_squares > 1250: # 50% of the square is applicable
@@ -29,21 +29,21 @@ for i in np.arange(0, 2345-50, 14):
             water_square = np.copy(water[i:i+50, j:j+50])
             temps_square = zerocenter(np.copy(temps[i+13:i+37, j+13:j+37]))
             lastcombinedsquare = None
-            for k in range(1):
+            for k in range(4):
                 combined_square = np.stack((pdensity_square, vegetation_square, water_square, applicable_square), axis=-1)
                 combined_squareT = np.stack((pdensity_square.T, vegetation_square.T, water_square.T, applicable_square.T), axis=-1)
                 inputs.append(combined_square)
                 outputs.append(temps_square)
                 inputs.append(combined_squareT)
                 outputs.append(temps_square.T)
-                # pdensity_square = np.rot90(pdensity_square)
-                # vegetation_square = np.rot90(vegetation_square)
-                # water_square = np.rot90(water_square)
-                # temps_square = np.rot90(temps_square)
-                # applicable_square = np.rot90(applicable_square)
+                pdensity_square = np.rot90(pdensity_square)
+                vegetation_square = np.rot90(vegetation_square)
+                water_square = np.rot90(water_square)
+                temps_square = np.rot90(temps_square)
+                applicable_square = np.rot90(applicable_square)
 
 
-random.shuffle(inputs)
-random.shuffle(outputs)
+# random.shuffle(inputs)
+# random.shuffle(outputs)
 np.save("inputs.npy", inputs)
 np.save("outputs.npy", outputs)
